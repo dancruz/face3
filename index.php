2b7395207a5dff97fbf7180f2317dd616cc33dfd
Dan Cruz <daniel.cruzry@gmail.com>
2014-11-22 00:20:19 -0600

    Creacion de archivo
    Archivo index.php

---
+++ index.php
@@ -0,0 +1,11 @@
+<html>
+<head>
+<title>Proyecto Sistemas Inteligentes</title>
+</head>
+<body>
+<ul>
+<li><a href="publicacion.php">Publicaciones</a></li>
+<li><a href="comentario.php">Comentarios</a></li>
+</ul>
+</body>
+</html>
